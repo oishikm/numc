ce460eb49cd966ba6583ea62fe8649c2ff93804a
Oishik Mukhopadhyay <oishik.mukhopadhyay@gmail.com>
2022-04-24 16:28:54 +0530

    refactor lib rot90

--- libnumc/libnumc.h
+++ libnumc/libnumc.h
@@ -31,36 +31,35 @@ int** _reshape2d(int **inarr, int r, int c, int nr, int nc)
 {
 	/* r = rows, c = columns */
 	/* nr = new rows, nc = new columns */
-	if(r*c == nr*nc)
-	{	
-		register int i, j, k;
-		
-		int* temp1d = (int *)malloc(r * c * sizeof(int));				
-		int** outarr = (int **)malloc(nr * sizeof(int *));		
-		for(i=0; i<nr; i++)
-		{
-			outarr[i] = (int *)malloc(nc * sizeof(int));
-		}
 
-		for(i=0, k=0; i<r; i++)
-			for(j=0; j<c; j++)
-				temp1d[k++] = inarr[i][j];
-		
-		for(i=0, k=0; i<nr; i++)
-			for(j=0; j<nc; j++)
-				outarr[i][j] = temp1d[k++];
-		
-		return outarr;
-	}
-	else
+	if(r*c != nr*nc)
 	{
 		char msg[200];
 		sprintf(msg, "Error: Input and Output arrays do not have equal size."
 				"\n[INFO] Input array has size %d elements while Output array has %d elements.\n", 
 				r*c, nr*nc);
 		numc_exception(msg);		
+	}	
+
+	register int i, j, k;
+	
+	int* temp1d = (int *)malloc(r * c * sizeof(int));				
+	int** outarr = (int **)malloc(nr * sizeof(int *));	
+
+	for(i=0; i<nr; i++)
+	{
+		outarr[i] = (int *)malloc(nc * sizeof(int));
 	}
-	return NULL; /* Control should not reach here, this line is to satisfy gcc (warning on -Wall) on linux */
+
+	for(i=0, k=0; i<r; i++)
+		for(j=0; j<c; j++)
+			temp1d[k++] = inarr[i][j];
+	
+	for(i=0, k=0; i<nr; i++)
+		for(j=0; j<nc; j++)
+			outarr[i][j] = temp1d[k++];
+	
+	return outarr;
 }
 
 int** _zeros2d(int r, int c)
